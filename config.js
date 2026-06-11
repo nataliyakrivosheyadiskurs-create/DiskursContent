09509c56dfa53ca101dee284698c176a9c5ca142
nataliyakrivosheyadiskurs-create <nataliya.krivosheya.diskurs@gmail.com>
2026-06-11 17:18:15 +0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// =====================================================
-// КОНФИГ — ЗАМЕНИ API_URL ПОСЛЕ ПУБЛИКАЦИИ Apps Script
-// =====================================================
-const CONFIG = {
-  // Вставь сюда URL из «Развернуть → Управление развёртываниями»
-  API_URL: 'https://script.google.com/macros/s/AKfycbwBa7VLwwwsKWyopBjgZSZSaOW53S4CBTz5fELjmeRo_xFqMvCDlzvWlaUr9-1x7fBn/exec',
-};
